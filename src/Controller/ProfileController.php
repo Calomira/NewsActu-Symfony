b8dff21138b0ca3b51e58ed970a7cbcf202dcc11
Calomira <zlatomira_bg@abv.bg>
2022-03-25 09:24:36 +0100

    composer require bundle

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,32 +2,32 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Commentary;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileController extends AbstractController
 {
-       /**
-        * @Route("/profile", name="show_profile", methods={"GET"})
-        */
-       public function showProfile(): Response
-       {
-            return $this->render('profile/show_profile.html.twig');
-       }
+     /**
+      * @Route("/profile", name="show_profile", methods={"GET"})
+      */
+     public function showProfile(): Response
+     {
+          return $this->render('profile/show_profile.html.twig');
+     }
 
+     /**
+      * @Route("/profile/tous-mes-commentaires", name="show_user_commentaries", methods={"GET"})
+      */
+     public function showUserCommentaries(EntityManagerInterface $entityManager): Response
+     {
+          $commentaries = $entityManager->getRepository(Commentary::class)->findBy(['author' => $this->getUser()]);
 
-       /**
-        * @Route("/profile/tous-mes-commentaires", name="show_user_commentaires", methods={"GET"})
-        */
-       public function showUserCommentaries(EntityManagerInterface $entityManager): Response 
-       {
-            $commentaries = $entityManager->getRepository(Commentary::class)->findBy(['author' => $this->getUser()]);
-
-            return $this->render('profile/show_user_commentaries.html.twig',[
-                 'commentaries => $commentaries'
-            ]);
-       }
+          return $this->render('profile/show_user_commentaries.html.twig', [
+               'commentaries' => $commentaries
+          ]);
+     }
 }
